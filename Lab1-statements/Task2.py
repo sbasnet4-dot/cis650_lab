6723260861a53b19284318386b5e802cb18434b7
Sujan Basnet <sbasnet4@murraystate.edu>
2025-08-25 11:44:38 -0500

    Done with task 2

---
+++ Lab1-statements/Task2.py
@@ -0,0 +1,7 @@
+Quantity= int(input('Enter number of bags'))
+Price = float (input('Enter price per bag'))
+print('Total Cost :', Quantity * Price)
+
+miles_driven = int(input(' Enter the Miles Driven : '))
+gallons_used = float(input(' Enter the Gallons Used :'))
+print(' Miles per Gallon :', miles_driven / gallons_used)
