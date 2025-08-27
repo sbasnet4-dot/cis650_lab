cb28b041f103118a814ed2a3262a70b0ef398933
Sujan Basnet <sbasnet4@murraystate.edu>
2025-08-27 11:03:28 -0500

    Done with Task 3

---
+++ Lab1-statements/Task3.py
@@ -0,0 +1,16 @@
+name = input('what is your name ?')
+if len(name)> 0:
+    print('Hello', name)
+else :
+    print ('enter your name,please!')
+
+
+
+
+First_Name = input('Enter your first name?')
+Last_Name = input(' Enter your last name ?')
+name = First_Name + ' ' + Last_Name
+if len ( First_Name) & len (Last_Name) > 0 :
+    print('Hello', name)
+else:
+    print ('Enter your Name, please!')
